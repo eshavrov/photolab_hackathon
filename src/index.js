3a9dd0a40ec4c08b43224e9b476acffe37c19feb
eshavrov <shavrov.ep@gmail.com>
2019-03-08 02:30:47 +0300

    Added decodePNGFile

--- src/index.js
+++ src/index.js
@@ -1,28 +1,48 @@
 var fs = require('fs');
 const crc32 = require('../utils/crc32');
 
+const CHUNK_NAMES = {
+  TEXT: 'xxXx',
+  IMG: 'ixXx',
+  OPTIONS: 'OPTS',
+};
+
+const encode = (data, key) => data;
+
+const decode = (data, key) => data;
+
 // https://www.w3.org/TR/PNG/#5Chunk-layout
-const createChunk = (name, data) => {
+const createChunk = (name, data, key = 'default') => {
   const len = Buffer.allocUnsafe(4);
   len.writeInt32BE(data.length, 0);
-  const _data = Buffer.concat([Buffer.from(name), data]);
+  const _data = encode(Buffer.concat([Buffer.from(name), data]), key);
   const result = Buffer.concat([len, _data, crc32(_data)]);
 
   return result;
 };
 
-const addChunk = (data, chunk) => {
-  const start = data.length - 12;
-
-  return Buffer.concat([data.slice(0, start), chunk, data.slice(start, data.length)]);
+const addChunk = (data, ...chunks) => {
+  const start = data.indexOf('IEND') - 4;
+  return start > 0 ? Buffer.concat([data.slice(0, start), ...chunks, data.slice(start, data.length)]) : data;
 };
 
-const writeNewPNGFile = (path, newPath, { text, img }) => {
-  fs.readFile(path, function(err, data) {
+const writeNewPNGFile = (
+  path,
+  newPath,
+  { text, buffer, options = { format: 'png', filename: 'output.png' } },
+  key = 'default'
+) => {
+  fs.readFile(path, (err, data) => {
     if (err) throw err;
-    const buffer = Buffer.from(text);
-
-    fs.writeFile(newPath, addChunk(data, createChunk('xxXx', buffer)), function(err) {
+    let newwData = data;
+    if (text) {
+      newwData = addChunk(newwData, createChunk(CHUNK_NAMES.TEXT, Buffer.from(text), key));
+    }
+    if (buffer) {
+      const chunkOptions = createChunk(CHUNK_NAMES.OPTIONS, Buffer.from(JSON.stringify(options)), key);
+      newwData = addChunk(newwData, createChunk(CHUNK_NAMES.IMG, Buffer.concat([chunkOptions, buffer]), key));
+    }
+    fs.writeFile(newPath, newwData, function(err) {
       if (err) {
         return console.log(err);
       }
@@ -31,8 +51,53 @@ const writeNewPNGFile = (path, newPath, { text, img }) => {
   });
 };
 
+const searchText = (data, key) => {
+  const start = data.indexOf(CHUNK_NAMES.TEXT) - 4;
+  if (start > 0) {
+    const len = data.readInt32BE(start);
+    const _buffer = data.slice(start + 8, start + 8 + len);
+    return decode(_buffer, key).toString('utf8');
+  }
+
+  return null;
+};
+
+const searchImage = (data, key) => {
+  const start = data.indexOf(CHUNK_NAMES.IMG) - 4;
+  if (start > 0) {
+    const len = data.readInt32BE(start);
+    const lenOptions = data.readInt32BE(start + 8);
+    const _opts = decode(data.slice(start + 16, start + 16 + lenOptions), key).toString('utf8') || '{}';
+    const _buffer = decode(data.slice(start + lenOptions + 20, start + 8 + len), key);
+
+    return {
+      ...JSON.parse(_opts),
+      data: _buffer,
+    };
+  }
+
+  return null;
+};
+
+const decodePNGFile = (path, key = 'default') => {
+  fs.readFile(path, (err, data) => {
+    if (err) throw err;
+    const dataImg = searchImage(data, key);
+    if (dataImg)
+      fs.writeFile(dataImg.filename, dataImg.data, function(err) {
+        if (err) {
+          return console.log(err);
+        }
+        console.log('The file was saved!');
+      });
+    console.log('text:', searchText(data, key));
+    console.log('img:', dataImg);
+  });
+};
+
 module.exports = {
   addChunk,
   createChunk,
   writeNewPNGFile,
+  decodePNGFile,
 };
--- start.js
+++ start.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 const crc32 = require('./utils/crc32');
 const writeNewPNGFile = require('./src').writeNewPNGFile;
+const decodePNGFile = require('./src').decodePNGFile;
 
 const pngSignature = Buffer.from([0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a]);
 
@@ -13,3 +14,28 @@ console.log(crc32(buf), ' === AE 42 60 82');
 writeNewPNGFile('./stickers/sticker_vk_starwars_004.png', './new_sticker.png', {
   text: 'hello world',
 });
+
+fs.readFile('./stickers/sticker_vk_starwars_000.png', (err, data) => {
+  if (err) throw err;
+  writeNewPNGFile('./stickers/sticker_vk_starwars_001.png', './new_sticker_pic1.png', {
+    options: {
+      filename: 'sticker1.png',
+      format: 'png',
+    },
+    buffer: data,
+  });
+});
+
+fs.readFile('./stickers/sticker_vk_starwars_002.png', (err, data) => {
+  if (err) throw err;
+  writeNewPNGFile('./stickers/sticker_vk_starwars_003.png', './new_sticker_pic2.png', {
+    buffer: data,
+    options: {
+      filename: 'sticker2.png',
+      format: 'png',
+    },
+    text: 'test text',
+  });
+});
+
+// decodePNGFile('./sticker_pic2.png');
